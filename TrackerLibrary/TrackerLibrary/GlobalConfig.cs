c6634f42a64bda7083090852e5f913f2a370369e
Brandon McGhee <bmcghee39@gmail.com>
2020-03-02 13:35:32 -0500

    started global config for sql connection and text file

---
+++ TrackerLibrary/TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                //TODO - initialize database
+            }
+
+            if (textFiles)
+            {
+                //TODO - create text file
+            }
+        }
+    }
+}
